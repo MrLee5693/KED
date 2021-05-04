a98a6ab2fb070a6c51a844b3728d2d5e6c35bc77
MrLee5693 <52459996+MrLee5693@users.noreply.github.com>
2021-05-04 18:11:52 +0900

    Hyperparameter Add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from item2vec import Item2Vec
 if __name__ == "__main__":
-    FILE_PATH = 'Your Path'
+    FILE_PATH = './'
     I2V = Item2Vec(FILE_PATH)
-    I2V.run()
+    I2V.run(size=128,class_name="세세분류",topk=5)
